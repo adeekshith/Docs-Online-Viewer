ad63d55f3ea39678a6041e737d64afdfccc4d7e3
Deekshith Allamaneni <dkh.hyd@gmail.com>
2015-11-10 22:32:23 -0600

    Changed DOV link icon.

--- js/127774.user.js
+++ js/127774.user.js
@@ -95,65 +95,17 @@ function changeLink(link, fileTypeCategory, fileExtension) {
 	//viewLink.docView=true; -> This line is removed in this version but still doubt if it can really be removed.
 	viewLink.title="View this \""+fileExtension+"\" file";
 	var ico = document.createElement("img");
-	ico.src =  'data:image/png;base64,' +
-		'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAKT2lDQ1BQaG90b3Nob3AgSUNDIHByb2Zp' +
-		'bGUAAHjanVNnVFPpFj333vRCS4iAlEtvUhUIIFJCi4AUkSYqIQkQSoghodkVUcERRUUEG8igiAOOjoCM' +
-		'FVEsDIoK2AfkIaKOg6OIisr74Xuja9a89+bN/rXXPues852zzwfACAyWSDNRNYAMqUIeEeCDx8TG4eQu' +
-		'QIEKJHAAEAizZCFz/SMBAPh+PDwrIsAHvgABeNMLCADATZvAMByH/w/qQplcAYCEAcB0kThLCIAUAEB6' +
-		'jkKmAEBGAYCdmCZTAKAEAGDLY2LjAFAtAGAnf+bTAICd+Jl7AQBblCEVAaCRACATZYhEAGg7AKzPVopF' +
-		'AFgwABRmS8Q5ANgtADBJV2ZIALC3AMDOEAuyAAgMADBRiIUpAAR7AGDIIyN4AISZABRG8lc88SuuEOcq' +
-		'AAB4mbI8uSQ5RYFbCC1xB1dXLh4ozkkXKxQ2YQJhmkAuwnmZGTKBNA/g88wAAKCRFRHgg/P9eM4Ors7O' +
-		'No62Dl8t6r8G/yJiYuP+5c+rcEAAAOF0ftH+LC+zGoA7BoBt/qIl7gRoXgugdfeLZrIPQLUAoOnaV/Nw' +
-		'+H48PEWhkLnZ2eXk5NhKxEJbYcpXff5nwl/AV/1s+X48/Pf14L7iJIEyXYFHBPjgwsz0TKUcz5IJhGLc' +
-		'5o9H/LcL//wd0yLESWK5WCoU41EScY5EmozzMqUiiUKSKcUl0v9k4t8s+wM+3zUAsGo+AXuRLahdYwP2' +
-		'SycQWHTA4vcAAPK7b8HUKAgDgGiD4c93/+8//UegJQCAZkmScQAAXkQkLlTKsz/HCAAARKCBKrBBG/TB' +
-		'GCzABhzBBdzBC/xgNoRCJMTCQhBCCmSAHHJgKayCQiiGzbAdKmAv1EAdNMBRaIaTcA4uwlW4Dj1wD/ph' +
-		'CJ7BKLyBCQRByAgTYSHaiAFiilgjjggXmYX4IcFIBBKLJCDJiBRRIkuRNUgxUopUIFVIHfI9cgI5h1xG' +
-		'upE7yAAygvyGvEcxlIGyUT3UDLVDuag3GoRGogvQZHQxmo8WoJvQcrQaPYw2oefQq2gP2o8+Q8cwwOgY' +
-		'BzPEbDAuxsNCsTgsCZNjy7EirAyrxhqwVqwDu4n1Y8+xdwQSgUXACTYEd0IgYR5BSFhMWE7YSKggHCQ0' +
-		'EdoJNwkDhFHCJyKTqEu0JroR+cQYYjIxh1hILCPWEo8TLxB7iEPENyQSiUMyJ7mQAkmxpFTSEtJG0m5S' +
-		'I+ksqZs0SBojk8naZGuyBzmULCAryIXkneTD5DPkG+Qh8lsKnWJAcaT4U+IoUspqShnlEOU05QZlmDJB' +
-		'VaOaUt2ooVQRNY9aQq2htlKvUYeoEzR1mjnNgxZJS6WtopXTGmgXaPdpr+h0uhHdlR5Ol9BX0svpR+iX' +
-		'6AP0dwwNhhWDx4hnKBmbGAcYZxl3GK+YTKYZ04sZx1QwNzHrmOeZD5lvVVgqtip8FZHKCpVKlSaVGyov' +
-		'VKmqpqreqgtV81XLVI+pXlN9rkZVM1PjqQnUlqtVqp1Q61MbU2epO6iHqmeob1Q/pH5Z/YkGWcNMw09D' +
-		'pFGgsV/jvMYgC2MZs3gsIWsNq4Z1gTXEJrHN2Xx2KruY/R27iz2qqaE5QzNKM1ezUvOUZj8H45hx+Jx0' +
-		'TgnnKKeX836K3hTvKeIpG6Y0TLkxZVxrqpaXllirSKtRq0frvTau7aedpr1Fu1n7gQ5Bx0onXCdHZ4/O' +
-		'BZ3nU9lT3acKpxZNPTr1ri6qa6UbobtEd79up+6Ynr5egJ5Mb6feeb3n+hx9L/1U/W36p/VHDFgGswwk' +
-		'BtsMzhg8xTVxbzwdL8fb8VFDXcNAQ6VhlWGX4YSRudE8o9VGjUYPjGnGXOMk423GbcajJgYmISZLTepN' +
-		'7ppSTbmmKaY7TDtMx83MzaLN1pk1mz0x1zLnm+eb15vft2BaeFostqi2uGVJsuRaplnutrxuhVo5WaVY' +
-		'VVpds0atna0l1rutu6cRp7lOk06rntZnw7Dxtsm2qbcZsOXYBtuutm22fWFnYhdnt8Wuw+6TvZN9un2N' +
-		'/T0HDYfZDqsdWh1+c7RyFDpWOt6azpzuP33F9JbpL2dYzxDP2DPjthPLKcRpnVOb00dnF2e5c4PziIuJ' +
-		'S4LLLpc+Lpsbxt3IveRKdPVxXeF60vWdm7Obwu2o26/uNu5p7ofcn8w0nymeWTNz0MPIQ+BR5dE/C5+V' +
-		'MGvfrH5PQ0+BZ7XnIy9jL5FXrdewt6V3qvdh7xc+9j5yn+M+4zw33jLeWV/MN8C3yLfLT8Nvnl+F30N/' +
-		'I/9k/3r/0QCngCUBZwOJgUGBWwL7+Hp8Ib+OPzrbZfay2e1BjKC5QRVBj4KtguXBrSFoyOyQrSH355jO' +
-		'kc5pDoVQfujW0Adh5mGLw34MJ4WHhVeGP45wiFga0TGXNXfR3ENz30T6RJZE3ptnMU85ry1KNSo+qi5q' +
-		'PNo3ujS6P8YuZlnM1VidWElsSxw5LiquNm5svt/87fOH4p3iC+N7F5gvyF1weaHOwvSFpxapLhIsOpZA' +
-		'TIhOOJTwQRAqqBaMJfITdyWOCnnCHcJnIi/RNtGI2ENcKh5O8kgqTXqS7JG8NXkkxTOlLOW5hCepkLxM' +
-		'DUzdmzqeFpp2IG0yPTq9MYOSkZBxQqohTZO2Z+pn5mZ2y6xlhbL+xW6Lty8elQfJa7OQrAVZLQq2Qqbo' +
-		'VFoo1yoHsmdlV2a/zYnKOZarnivN7cyzytuQN5zvn//tEsIS4ZK2pYZLVy0dWOa9rGo5sjxxedsK4xUF' +
-		'K4ZWBqw8uIq2Km3VT6vtV5eufr0mek1rgV7ByoLBtQFr6wtVCuWFfevc1+1dT1gvWd+1YfqGnRs+FYmK' +
-		'rhTbF5cVf9go3HjlG4dvyr+Z3JS0qavEuWTPZtJm6ebeLZ5bDpaql+aXDm4N2dq0Dd9WtO319kXbL5fN' +
-		'KNu7g7ZDuaO/PLi8ZafJzs07P1SkVPRU+lQ27tLdtWHX+G7R7ht7vPY07NXbW7z3/T7JvttVAVVN1WbV' +
-		'ZftJ+7P3P66Jqun4lvttXa1ObXHtxwPSA/0HIw6217nU1R3SPVRSj9Yr60cOxx++/p3vdy0NNg1VjZzG' +
-		'4iNwRHnk6fcJ3/ceDTradox7rOEH0x92HWcdL2pCmvKaRptTmvtbYlu6T8w+0dbq3nr8R9sfD5w0PFl5' +
-		'SvNUyWna6YLTk2fyz4ydlZ19fi753GDborZ752PO32oPb++6EHTh0kX/i+c7vDvOXPK4dPKy2+UTV7hX' +
-		'mq86X23qdOo8/pPTT8e7nLuarrlca7nuer21e2b36RueN87d9L158Rb/1tWeOT3dvfN6b/fF9/XfFt1+' +
-		'cif9zsu72Xcn7q28T7xf9EDtQdlD3YfVP1v+3Njv3H9qwHeg89HcR/cGhYPP/pH1jw9DBY+Zj8uGDYbr' +
-		'njg+OTniP3L96fynQ89kzyaeF/6i/suuFxYvfvjV69fO0ZjRoZfyl5O/bXyl/erA6xmv28bCxh6+yXgz' +
-		'MV70VvvtwXfcdx3vo98PT+R8IH8o/2j5sfVT0Kf7kxmTk/8EA5jz/GMzLdsAAAAJcEhZcwAACxMAAAsT' +
-		'AQCanBgAAAAHdElNRQfcCRQRMwXipoy1AAACv0lEQVQoz22Sz0uTcRzHP99nPx57mI+4pdPhlKU+WzrW' +
-		'I6mwYS5MEdukg2QFaUM6mYdA2x/gRTqFJi5TRBZYDOlSB4MemBDNSGFoPLjHUKfTix7SPduj2/N91kFS' +
-		'D71v78P7/T68X2h8jiORoiWwnE4e7MdBlvJAqSw3GfU0SWASYZJQtAiTBCaRQiKsBoCnD1sjkUgotJBO' +
-		'pwEAAPi9PYZhfD6fwWCAS8p+Dajudj0hleOpqalsNsswjMvlslqtACAIQiQSKS0tLSkpOQ8om8vo1exC' +
-		'PLrAsqzX673cJ0kSx3Ecx3V3dzudzouF4orrvgder9dLURTP8+FwmOd5hJDJZGIYpqGhIRQKaTQas9l8' +
-		'tqC2WK45nc54PD4yMiIIgiiKsixTFGWz2Xp6etxu9+Dg4MTEhNlsLisrAwDV/d5+dHLo9/tX1oTaO497' +
-		'nw9/lG4ntaat2Or3L/M7OztNTU2NjY2hUMhutxO7UVVze9f8uzcY4/YXQU+r+94NemU3Q1pcYoVHU2Tb' +
-		'4t7yPN/W1lZXV8dxXJX6mNjYEI6OjmZmZkx6usZIhJdjzO5s4eprizEfmW5Ozn3a3t72+/25XI5l2cRe' +
-		'ggCAoaGh/Px8GXLPPsPk7PtwOLz97UN5AajV6rSqIBAInGX0er1GoyFqa+2xWCyVSrVb8w7/pHSMWxRF' +
-		'Q1m1nEMIUHWhotPpRkdH19fXx8bGjMVGVWd3X9+jzmAwWO+wJjMoQVRVujqLXb7YIVgLJC+TwxgTBGGz' +
-		'2aanp+uvEgQAUBQ1MDCwtLTUTP26ZT79fUz/3BRraPHl3Sv4nywWi8PhkE4k9fm1Ho/n4ODgdO3HsINi' +
-		'WVZRlNPTtCzLyWQyGo1mMpn+/v7q/UX1ZbaKiopaWloAQJZljHEqlUokEjRNd3R0UBQFANn9RTQ+x2mR' +
-		'ck4vSeAL+z+8/wIdmkV4sAxf6AAAAABJRU5ErkJggg==';
+	ico.src =  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8' +
+				'/9hAAAA1ElEQVQ4y2NgoBYQq' +
+				'/3tL1r7uwEdC9f9iSbKAJG635Oxitf+Pi5a8yeOoAEg23CJi9b9Khep' +
+				'/WVLlgFAl3UyMPxnBBqyUqTmhyrJBqCHDckGEK2GNgbU' +
+				'/2cRrfvdDMX9QDVNJBkAij5g6BtDeMCArP2VI1r385d4' +
+				'/T8xggYI1fzQBMbAdJBGZHH++v8CQNecE6n' +
+				'/oYbXAKDNVeLF' +
+				'/7ixuVa49B8vKG3gNUC4' +
+				'/ocGKHViJO+6342iNb+vi9Z' +
+				'/V6FyLNT8/I0tM8EwME9ME6n' +
+				'/7cNAbQAAkE+TQSL7LkoAAAAASUVORK5CYII=';
 	//"data:image/png;base64,iVBORw0K...." This is a Data URI. This approach is better than using external URL.
 	// Adjusts the margin of the icon to the given number of pixels (3 to 5px is advisable)
 	ico.style.marginLeft = "3px";
