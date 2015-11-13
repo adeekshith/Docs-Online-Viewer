ca0d7ee85c1d69984020e6a218b8bd6d0ba08b91
Deekshith Allamaneni <dkh.hyd@gmail.com>
2015-11-13 01:00:10 -0600

    Builder.sh removes previous build/ directory everytime before building and has variable for zip file name.

--- tools/builder.sh
+++ tools/builder.sh
@@ -2,6 +2,12 @@
 
 echo "Building Docs Online Viewer to build/"
 
+# Zip file name
+outputZipFile="dov-current"
+
+# Removed all previous uncompressed files
+rm -r ../build/
+
 # Copy files from src/ to build/uncompressed/
 for f in $(find ../src -name '*.*' -not -name '.*'); do
 	dest=`echo "$f" | sed 's:^../src/:../build/uncompressed/:'`;
@@ -24,7 +30,7 @@ done
 
 # Zip all source files
 cd ../build/uncompressed
-zip -r -9 ../dov-current.zip *
+zip -r -9 ../$outputZipFile.zip *
 
 # Create copy of zip file with .xpi extension
-cp ../dov-current.zip ../dov-current.xpi
+cp ../$outputZipFile.zip ../dov-current.xpi
