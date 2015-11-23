a98c42e84a4b4a09377fba194bc1b6e715ed1f24
Deekshith Allamaneni <dkh.hyd@gmail.com>
2015-11-23 17:43:30 -0600

    Implementing JSON based configurations for user options. Opening icon beside link in new tab is implemented.

--- src/js/core/options.js
+++ src/js/core/options.js
@@ -1,26 +1,55 @@
+
+var userPrefJSON_default =
+    '{                                                      \
+        "application" : {                                   \
+            "name" : "Docs Online Viewer",                  \
+            "app_id" : "docs-online-viewer@deekshith.in",   \
+            "version" : "v5.5.6"                            \
+        },                                                  \
+        "user_preferences" : {                              \
+            "version" : "0.1",                              \
+            "icon_beside_doc_links" : {                     \
+                "enabled" : true,                           \
+                "icon_path": "images/beside-link-icon.png", \
+                "newtab_on_click" : false                   \
+            }                                               \
+        }                                                   \
+     }';
+
+
+function flash_options_status(message, time) {
+    var status = document.getElementById('status');
+    status.textContent = message;
+    setTimeout(function () {
+        status.textContent = '';
+    }, time);
+}
+
+
 // Saves options to chrome.storage.sync.
 function save_options() {
     var dovIconNewtab = document.getElementById('pref-dov-icon-newtab').checked;
+    var thisUserPreferences = JSON.parse(userPrefJSON_default);
+    thisUserPreferences.user_preferences.icon_beside_doc_links.newtab_on_click = dovIconNewtab;
+    var thisUserPreferencesStr = JSON.stringify(thisUserPreferences);
     chrome.storage.sync.set({
-        dovIconNewtab: dovIconNewtab
+        user_config:thisUserPreferencesStr
     }, function () {
         // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 750);
+        flash_options_status('Options saved.', 750);
     });
 }
+
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
     // Default value of dovIconNewtab = false.
     chrome.storage.sync.get({
-        dovIconNewtab: false
+        user_config: userPrefJSON_default
     }, function (items) {
-        document.getElementById('dov-icon-newtab').checked = items.dovIconNewtab;
+        var thisUserPreferences = JSON.parse(items.user_config);
+        document.getElementById('pref-dov-icon-newtab').checked = thisUserPreferences.user_preferences.icon_beside_doc_links.newtab_on_click;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
