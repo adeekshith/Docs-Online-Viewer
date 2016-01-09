217accdf1c9f7b490f6acf1f4a0790f6f9614ce5
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-01-09 12:41:03 -0600

    Performance: Simplified parameter validation config.js

--- src/js/core/config.js
+++ src/js/core/config.js
@@ -14,12 +14,9 @@ UserConfig.prototype.isIconBesideDocLinksEnabled = function() {
     return this.userPreferencesJSON_.user_preferences.icon_beside_doc_links.enabled;
 };
 UserConfig.prototype.setIconBesideDocLinksEnable = function (userInput) {
-    if(typeof(userInput) === "boolean") {
-        this.userPreferencesJSON_.user_preferences.icon_beside_doc_links.enabled = userInput;
-        return true;
-    }else {
-        return false;
-    }
+    if(typeof(userInput) !== "boolean") {console.log("Invalid input params"); return false;}
+    this.userPreferencesJSON_.user_preferences.icon_beside_doc_links.enabled = userInput;
+    return true;
 };
 UserConfig.prototype.getBesideDocLinksIconPath = function() {
     return this.userPreferencesJSON_.user_preferences.icon_beside_doc_links.icon_path;
@@ -28,12 +25,9 @@ UserConfig.prototype.isIconClickNewtab = function() {
     return this.userPreferencesJSON_.user_preferences.icon_beside_doc_links.newtab_on_click;
 };
 UserConfig.prototype.setIconClickNewtab = function (userInput) {
-    if(typeof(userInput) === "boolean") {
-        this.userPreferencesJSON_.user_preferences.icon_beside_doc_links.newtab_on_click = userInput;
-        return true;
-    }else {
-        return false;
-    }
+    if(typeof(userInput) !== "boolean") {console.log("Invalid input params"); return false;}
+    this.userPreferencesJSON_.user_preferences.icon_beside_doc_links.newtab_on_click = userInput;
+    return true;
 };
 UserConfig.prototype.isFiletypeEnabled = function (fileType) {
     let thisUserPrefJSON = this.userPreferencesJSON_;
@@ -44,21 +38,17 @@ UserConfig.prototype.isFiletypeEnabled = function (fileType) {
     });
 };
 UserConfig.prototype.setFiletypeEnable = function (fileType, userInput) {
-    if(typeof(userInput) === "boolean") {
-        let indexFiletype = this.userPreferencesJSON_.user_preferences.file_types
-            .findIndex( (thisFileTypeObj) => thisFileTypeObj.extension === fileType );
-        this.userPreferencesJSON_.user_preferences.file_types[indexFiletype].is_enabled = userInput;
-    }else {
-        return false;
-    }
+    if(typeof(userInput) !== "boolean") {console.log("Invalid input params"); return false;}
+    let indexFiletype = this.userPreferencesJSON_.user_preferences.file_types
+        .findIndex( (thisFileTypeObj) => thisFileTypeObj.extension === fileType );
+    this.userPreferencesJSON_.user_preferences.file_types[indexFiletype].is_enabled = userInput;
 };
 UserConfig.prototype.getPrivacyCollectStatsStatus = function () {
     return this.userPreferencesJSON_.user_preferences.privacy.collect_stats;
 };
 UserConfig.prototype.setPrivacyCollectStatsStatus = function (userInput) {
-    if(typeof(userInput) === "boolean") {
-        this.userPreferencesJSON_.user_preferences.privacy.collect_stats = userInput;
-    }
+    if(typeof(userInput) !== "boolean") {console.log("Invalid input params"); return false;}
+    this.userPreferencesJSON_.user_preferences.privacy.collect_stats = userInput;
 };
 UserConfig.prototype.getPreferences = function () {
     return this.userPreferencesJSON_;
