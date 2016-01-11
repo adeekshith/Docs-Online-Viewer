ebe914e5c0243c60be4622a34434ad619e3aa4f6
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-01-11 17:39:21 -0600

    Moved textFileLoad() Promised based function moved to dov-utils.js

--- src/js/core/options.js
+++ src/js/core/options.js
@@ -4,36 +4,6 @@
 
 "use strict";
 
-function textFileLoad(url) {
-    // Create new promise with the Promise() constructor;
-    // This has as its argument a function
-    // with two parameters, resolve and reject
-    return new Promise(function(resolve, reject) {
-        // Standard XHR to load json
-        var request = new XMLHttpRequest();
-        request.open('GET', url, true);
-        request.responseType = 'text';
-        // When the request loads, check whether it was successful
-        request.onload = function() {
-            if (request.status === 200) {
-                // If successful, resolve the promise by passing back the request response
-                resolve(request.responseText);
-            } else {
-                // If it fails, reject the promise with a error message
-                reject(Error('Give file didn\'t load successfully; error code:' + request.statusText));
-            }
-        };
-        request.onerror = function() {
-            // Also deal with the case when the entire request fails to begin with
-            // This is probably a network error, so reject the promise with an appropriate message
-            reject(Error('There was a network error.'));
-        };
-        // Send the request
-        request.send();
-    });
-}
-
-
 function flash_options_status(message, time) {
     let status = document.getElementById('status');
     let saveButton = document.getElementById('dov_save');
