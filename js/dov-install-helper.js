70ea4a9d9b7766f7c6a6683695ed64cc9b0bf1c4
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-01-24 11:01:08 -0600

    Unsupported browser icons are muted.

--- js/dov-install-helper.js
+++ js/dov-install-helper.js
@@ -10,16 +10,30 @@ function getDovUrlForBrowser(browserName) {
         case "Chrome":
             browserUrl = "https://chrome.google.com/webstore/detail/docs-online-viewer/gmpljdlgcdkljlppaekciacdmdlhfeon";
             break;
-        case "Opera":
-            browserUrl = "https://addons.opera.com/en/extensions/details/docs-online-viewer/";
-            break;
         case "Firefox":
             browserUrl = "https://addons.mozilla.org/en-US/firefox/addon/docs-online-viewer/";
+            break;
+        case "Opera":
+            browserUrl = "https://addons.opera.com/en/extensions/details/docs-online-viewer/";
             break;
         default:
             browserUrl = null;
     }
     return browserUrl;
+}
+
+function enableSupportedBrowserIcon(browserName) {
+    switch (browserName) {
+        case "Chrome":
+            document.getElementById("supported-browser-icon-chrome").classList.remove("text-muted");
+            break;
+        case "Firefox":
+            document.getElementById("supported-browser-icon-firefox").classList.remove("text-muted");
+            break;
+        case "Opera":
+            document.getElementById("supported-browser-icon-opera").classList.remove("text-muted");
+            break;
+    }
 }
 
 function changeDovInstallButton() {
@@ -34,6 +48,7 @@ function changeDovInstallButton() {
     installButtonElement.classList.remove("disabled");
     installButtonElement.href = thisBrowserUrl;
     installButtonElement.innerHTML = "Install for ".concat(thisBrowserName);
+    enableSupportedBrowserIcon(thisBrowserName);
 }
 
 changeDovInstallButton();
