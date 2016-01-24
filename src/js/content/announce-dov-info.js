dd5a9d70db6ad3db84b084d24272acaa61de402b
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-01-24 13:21:02 -0600

    Changes install button to show it is already installed.

--- src/js/content/announce-dov-info.js
+++ src/js/content/announce-dov-info.js
@@ -1,5 +1,16 @@
 /**
  * Created by Deekshith Allamaneni on 1/24/16.
  */
+"use strict";
 
-var dovInstalled = true;
+let homepageInstallBtnId = "dov-install-button";
+
+function changeHomepageDovInstallBtnSuccess(homepageInstallBtnId) {
+    let homepageInstallBtnElement = document.getElementById(homepageInstallBtnId);
+    if (homepageInstallBtnElement === null) {return;}
+    homepageInstallBtnElement.innerHTML = "Installed. Take a moment to rate it.";
+    homepageInstallBtnElement.classList.remove("btn-primary");
+    homepageInstallBtnElement.classList.add("btn-success");
+}
+
+changeHomepageDovInstallBtnSuccess(homepageInstallBtnId);
