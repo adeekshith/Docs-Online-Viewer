cafbff8e7ad310c028a5ea1d14fe802bd869cd6e
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-01-24 09:53:13 -0600

    Browser detect library returns null for unsupported browsers.

--- libs/browser-detect.js
+++ libs/browser-detect.js
@@ -11,9 +11,12 @@ function detectBrowser(userAgent) {
             userAgent.match(RegExp('\\b('+browserName+')\\/[0-9.]+', 'g')))
         .filter( thisMatch =>
         thisMatch !== null);
-    let browserInfo = userAgentMatches.length >= 0
+    let browserInfo = userAgentMatches.length > 0
         ? userAgentMatches[0].toString() // Ex: Firefox/45.02
         : null;
+    if(browserInfo === null) {
+        return {browser_name: null, browser_version: null};
+    }
     let browserName = browserInfo.split("/")[0];
     browserName = (browserName === "OPR") ? "Opera" : browserName;
     return {browser_name: browserName, browser_version: browserInfo.split("/")[1]};
