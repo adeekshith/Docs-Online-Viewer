d3855c6825d8fcf9291d2a33bb26860bbb132596
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-01-24 13:47:30 -0600

    Installed text changed in install announce

--- src/js/content/announce-dov-info.js
+++ src/js/content/announce-dov-info.js
@@ -8,7 +8,7 @@ let homepageInstallBtnId = "dov-install-button";
 function changeHomepageDovInstallBtnSuccess(homepageInstallBtnId) {
     let homepageInstallBtnElement = document.getElementById(homepageInstallBtnId);
     if (homepageInstallBtnElement === null) {return;}
-    homepageInstallBtnElement.innerHTML = "<i class=\"fa fa-check fa-fw\"></i> Installed. Take a moment to rate it.";
+    homepageInstallBtnElement.innerHTML = "<i class=\"fa fa-check fa-fw\"></i>Installed. Rate it <i class=\"fa fa-smile-o fa-fw\"></i>";
     homepageInstallBtnElement.classList.remove("btn-primary");
     homepageInstallBtnElement.classList.add("btn-success");
 }
