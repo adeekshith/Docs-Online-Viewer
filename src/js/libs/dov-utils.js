7fd9ad92af1db38eeec3e67c3cc633822cc611e4
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-02-14 15:27:41 -0600

    refactor: Moved getUrlContentType() to utils, loading utils js in background.

--- src/js/libs/dov-utils.js
+++ src/js/libs/dov-utils.js
@@ -5,6 +5,39 @@
 
 "use strict";
 
+function getUrlContentType(url) {
+    // Create new promise with the Promise() constructor;
+    // This has as its argument a function
+    // with two parameters, resolve and reject
+    return new Promise(function(resolve, reject) {
+        // Standard XHR
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState == 2) {
+                resolve(
+                    {
+                        url: url,
+                        content_type: request.getResponseHeader("Content-Type"),
+                        status: request.status
+                    }
+                );
+            }
+        };
+        request.onerror = function() {
+            // Also deal with the case when the entire request fails to begin with
+            // This is probably a network error, so reject the promise with an appropriate message
+            reject(Error('Network error reading Content-Type.'));
+        };
+        request.ontimeout = function () {
+            reject(Error('Request timeout'));
+        };
+        request.open("GET", url, true);
+        request.timeout = 2000; // Timeout in ms
+        request.send(null);
+    });
+}
+
+
 function textFileLoad(url) {
     // Create new promise with the Promise() constructor;
     // This has as its argument a function
