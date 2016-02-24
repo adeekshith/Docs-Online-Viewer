c2d0a79f5c426ee98abdb3689a3571a6166b6717
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-02-23 19:41:18 -0600

    Added feedback section below getting started section.

--- js/dov-install-helper.js
+++ js/dov-install-helper.js
@@ -52,3 +52,32 @@ function changeDovInstallButton() {
 }
 
 changeDovInstallButton();
+
+// User Feedback Section
+function userLikesDOVView() {
+    document.getElementById('dov-options-feedback').innerHTML =
+        `<a class=\"btn btn-success\" href=${getDovUrlForBrowser(detectBrowser(navigator.userAgent).browser_name)} target=\"_blank\">
+            <i class=\"fa fa-thumbs-up fa-2x pull-left\"></i> &nbsp;&nbsp;&nbsp;Rate it&nbsp;&nbsp;&nbsp;<br/>
+            <i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i>
+        </a>
+        <br/>
+        <div class=\"text-muted\">Share it</div>
+        <a style=\"color: #3b5998\" href=\"https://www.facebook.com/sharer/sharer.php?u=http://dov.parishod.com/\"><i class=\"fa fa-facebook-square fa-2x\"></i></a>
+        <a style=\"color: #55acee\" href=\"https://twitter.com/share?text=View%20any%20file%20in%20your%20browser%20with%20Docs%20Online%20Viewer.&url=http://dov.parishod.com/\"><i class=\"fa fa-twitter-square fa-2x\"></i></a>
+        <a style=\"color: #d34836\" href=\"https://plus.google.com/share?url=http://dov.parishod.com/\"><i class=\"fa fa-google-plus-square fa-2x\"></i></a>
+        `;
+}
+
+
+function userDislikesDOVView() {
+    document.getElementById('dov-options-feedback').innerHTML =
+        "<h4>Report Issues</h4>\
+        <div class=\"text-center\">\
+            <a class=\"btn btn-warning\" href=\"https://github.com/adeekshith/Docs-Online-Viewer/issues\" target=\"_blank\">    \
+                    <i class=\"fa fa-bug fa-2x pull-left\"></i> Report<br/>issues</a> \
+            <br/>Sorry to know you have issues with it. Report them and we will try to resolve them as soon as possible. Thank you.\
+        </div>";
+}
+
+document.getElementById('user-liked-dov-options-feedback').addEventListener('click', userLikesDOVView);
+document.getElementById('user-disliked-dov-options-feedback').addEventListener('click', userDislikesDOVView);
